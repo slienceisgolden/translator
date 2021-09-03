060d76f65511815a9f5c8892a91e32294fd17482
Ivan Shipilov <plus-invest@yandex.ru>
2021-09-04 03:52:01 +0500

    1

--- parser.js
+++ parser.js
@@ -4,6 +4,7 @@ const db = require('better-sqlite3')('data/db-source.sqlite');
 const async = require('async');
 
 // db.prepare('DELETE FROM pages').run();
+db.prepare('UPDATE pages SET status = 1 WHERE status = 2').run();
 
 // Сборщик списка страниц
 // (async () => {
@@ -79,5 +80,51 @@ const async = require('async');
 // })()
 
 // парсер страниц
-const pages = db.prepare('SELECT src FROM pages WHERE status = 1').all();
-console.log(pages[0]);
+const pages = db.prepare('SELECT id, src FROM pages WHERE status = 1').all();
+let counter = 0;
+async.eachLimit(pages.slice(0, 10), 5, (link, callback) => {
+    (async () => {
+        try {
+            const page = await axios.get(link.src);
+            const $ = cheerio.load(page.data);
+            let html = $('#editorial-body').first();
+            html.find('script, noscript, iframe, google-read-aloud-player, .hidden, .ad-disclaimer, .sticky-container, .ad-inline').remove();
+            html.find('picture').each(function (ind, el) {
+                let src = $(this).find('img').first().attr('src')
+                let data = $(this).find('img').first().attr('data-src')
+                if (data) src = data;
+                if (!src) {
+                    $(this).remove();
+                    return false;  
+                } 
+                $(this).replaceWith(`<img src='${src}'>`)
+            })
+            html.find('a, span').each(function (ind, el) {
+                $(this).replaceWith($(this).html());
+            });
+            html.find('p').each(function (ind, el) {
+                if($(this).find('img').length) return false;
+                if($(this).text().trim() == '')
+                    $(this).remove();
+            });
+            for (let iterator; iterator < 5; iterator++)
+                html.find('div').each(function (ind, el) {
+                    if($(this).find('img').length) return false;
+                    if($(this).text().trim() == '')
+                        $(this).remove();
+                });
+            html.find('*').removeAttr('id class style')
+            text = html.html().trim();
+            const title = $('title').first().text().replace(/\|.*/i,'').trim()
+            db.prepare('UPDATE pages SET status = 2, text = ?, title = ? WHERE id = ?').run(text,title,link.id);
+        } catch (error) {
+            console.log('ошибка парсинга', error);
+        }
+        counter++;
+        if (counter % 10 == 0)
+            console.log('Обработано (%, всего):', Math.round(100*counter/pages.length), counter);
+        callback();
+    })();
+},() => {
+    console.log('done');
+} );
